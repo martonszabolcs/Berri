9155aea04a34403e8c52b82d0828ab9f738c5cff
Kiss Veronika <weruss.kiss@gmail.com>
2025-11-26 17:06:56 +0100

    berri url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  // BASE_URL: "https://app.berribook.com", 
-  BASE_URL: __DEV__ ? 'http://localhost:3000' : 'https://api.yourdomain.com',
+  BASE_URL: "https://app.berribook.com", 
+  // BASE_URL: __DEV__ ? 'http://localhost:3000' : 'https://api.yourdomain.com',
   TIMEOUT: 10000,
 };
 
